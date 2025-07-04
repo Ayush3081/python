baac8455c6f431b02da6a50c8a4a461415d19e84
Ayush3081 <ayushsoni4257@gmail.com>
2025-07-04 14:30:19 +0530

    basic.py

--- basic.py
+++ basic.py
@@ -1,28 +1,3 @@
-# print(5&6)
-# print(5|6)
-# print(5^6)
-# print(5<<6)
-# print(5>>6)
-
-# x = 5
-# print(x*2+3) 
-
-# x = 5
-# x=x+1
-# print(x*(3+2))
-
-# x = input("Enter a number: ")
-# y = input("Enter another number: ")
-# z =( .5*float(x)*float(y))
-# print("The area of the triangle is: ", z)
-
-# x = input("Enter a number of x : ")
-# y = input("Enter another number of y : ")
-# x,y = y,x
-# print("x =",x)
-# print("y =",y) 
-
-
 mango_price = 40
 banana_price = 50
 pineapple_price = 30
@@ -47,22 +22,6 @@ print("Your discount is: ", discount)
 total_bill = bill - discount
 print("Your total bill after discount is: ", total_bill)
 
-# import math as m # To impport math modole with an alias
-# x = m.sqrt(16)
-# print("The square root of 12 is: ", x)
-
-# from math import * # To import all functions from the math module
-# x = gcd(16,12)
-# print("The square root of 16 is: ", x)
-
-# x = 2
-# y = 3
-# print(x,y ,sep="*") # To print x and y with a separator
-
-# x =10
-# y = 3.5
-# z = "Akanksha"
-# print("hello,{2} x = {0}, y = {1}".format(x, y, z))
 
 
 
